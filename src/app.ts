20745094a8954c69103a10f7fa015e6752090b7f
MD Tareq Mahmud <tareqmahmud@users.noreply.github.com>
2021-10-13 20:50:19 +0600

    Run express app for crud

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import { createConnection } from "typeorm";
+import express from "express";
 import * as dotenv from "dotenv";
 import Client from "./entities/Client";
 import Banker from "./entities/Banker";
@@ -7,12 +8,19 @@ import Transaction from "./entities/Transaction";
 // Enable dotenv
 dotenv.config();
 
+// Express app
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Connect DB
 const connectDB = async () => {
   try {
     await createConnection({
       type: "postgres",
       host: process.env.DB_HOST,
-      port: +process.env.DB_PORT,
+      port: 5432,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       synchronize: process.env.DB_SYNCHRONIZE === "true",
@@ -25,4 +33,10 @@ const connectDB = async () => {
   }
 };
 
-connectDB();
+app.listen(3000, async () => {
+  // Connect the DB
+  await connectDB();
+
+  // Show app running message
+  console.log("App is on fire 🔥");
+});
