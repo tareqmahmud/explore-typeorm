da7c2f5f7d4877dc138108c9ece8d4fdb45dcf17
MD Tareq Mahmud <tareqmahmud@users.noreply.github.com>
2021-10-13 23:20:36 +0600

    Get transactions from client

--- src/routes/client-route.ts
+++ src/routes/client-route.ts
@@ -1,10 +1,18 @@
 import express from "express";
 import Client from "../entities/Client";
+import { createQueryBuilder } from "typeorm";
 
 const router = express.Router();
 
-router.get("/api/clients", (req, res) => {
-  res.send("Hello World");
+router.get("/api/clients", async (req, res) => {
+  const clients = await createQueryBuilder("client")
+    .select("client")
+    .from(Client, "client")
+    .leftJoinAndSelect("client.transactions", "transactions")
+    .where("client.id = :clientId", { clientId: 7 })
+    .getOne();
+
+  return res.json(clients);
 });
 
 // create client
